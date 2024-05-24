6f65a96bf533829e6ee7104b002f902d969c8e69
Francesca Barletta <wallebar@gmail.com>
2024-05-24 16:40:16 +0200

    add table with bootstrap

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,15 +6,39 @@
 
 <div class="container">
     <h1>Trains</h1>
-    <ul>
+    
+<table class="table table-dark table-striped">
+  <thead>
+    <tr>
+      
+      <th scope="col">Azienda</th>
+      <th scope="col">Data di partenza</th>
+      <th scope="col">Stazione di partenza</th>
+      <th scope="col">Stazione di arrivo</th>
+      <th scope="col">Orario di partenza</th>
+      <th scope="col">Orario di arrivo</th>
+      <th scope="col">Codice Treno</th>
+      <th scope="col">Numero carrozze</th>
+    </tr>
+  </thead>
+  <tbody>
+   
 @foreach($trains as $train)
+    <tr> 
+      <td>{{ $train['azienda'] }}</td>
+      <td>{{ $train['data_di_partenza'] }}</td>
+      <td>{{ $train['stazione_di_partenza'] }}</td>
+      <td>{{ $train['stazione_di_arrivo'] }}</td>
+      <td>{{ $train['orario_di_partenza'] }}</td>
+      <td>{{ $train['orario_di_arrivo'] }}</td>
+      <td>{{ $train['codice_treno'] }}</td>
+      <td>{{ $train['numero_carrozze'] }}</td>
+    </tr>
+@endforeach
 
-        <li><p>{{ $train['azienda'] }}</p></li>
-        <li><p>{{ $train['data_di_partenza'] }}</p></li>
-        <li><p>{{ $train['stazione_di_partenza'] }}</p></li>
-        <li><p>{{ $train['stazione_di_arrivo'] }}</p></li>
- @endforeach
-    </ul>
+  </tbody>
+</table>
+
 </div>
 
 @endsection
